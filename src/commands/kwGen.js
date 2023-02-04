408223e1993a0402376ba321c29aca9d9614e02b
Kipras <kiprasjanuska0@gmail.com>
2023-02-04 16:30:35 -0500

    Add replacer functionality to kwgen command

--- src/commands/kwGen.js
+++ src/commands/kwGen.js
@@ -11,15 +11,35 @@ const data = new SlashCommandBuilder()
 	.addStringOption(option =>
 		option.setName("keywords")
 			.setDescription("List of keywords separated by spaces or commas")
-			.setRequired(true));
+			.setRequired(true))
+    .addStringOption(option =>
+        option.setName("find_what")
+            .setDescription("Keyword or character that you want to replace")
+            .setRequired(false))
+    .addStringOption(option =>
+        option.setName("replace_with")
+            .setDescription("What you want to replace with")
+            .setRequired(false));
 
 
 export const command = {
 	data: data,
 	async execute(interaction) {
 		const prefix = interaction.options.getString('prefix');
-		const keywords = interaction.options.getString('keywords').split(/[ ,]+/);
+		let keywords = interaction.options.getString('keywords').split(/[ ,]+/);
+        const find = interaction.options.getString('find_what');
+        const replace = interaction.options.getString('replace_with');
+        if (find !== null && replace === null) {
+            await interaction.reply("Error: You must provide a replacement");
+            return;
+        }
 		try {
+            if (find !== null && replace !== null) {
+                // replace() only replaces the first match by default
+                // adding the "g" flag makes it replace all matches
+                const regExp = new RegExp(find, "g");
+                keywords = keywords.map(keyword => keyword.replace(regExp, replace));
+            }
 			const prefixed = new AttachmentBuilder(Buffer.from(genKWs(prefix, keywords).join("\n"), 'utf-8'), { name: "prefixed.txt" });
 			await interaction.reply({ files: [prefixed], ephemeral: true });
 		} catch (error) {
